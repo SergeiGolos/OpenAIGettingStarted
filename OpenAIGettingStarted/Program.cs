77be56848080bed3db2209c0e3f219581f2d6945
Sergei Golos <sergei.golos@gmail.com>
2023-05-02 09:10:59 -0400

    cleaned up the code a bit

--- OpenAIGettingStarted/Program.cs
+++ OpenAIGettingStarted/Program.cs
@@ -1,29 +1,20 @@
 ﻿using Autofac;
 using OpenAI_API;
 using OpenAIGettingStarted;
+using OpenAIGettingStarted.Trainers;
 
 var builder = new ContainerBuilder();
 builder.RegisterModule<OpenAIModule>();
-
 var container = builder.Build();
-var api = container.Resolve<OpenAIAPI>();
-var training = new WorkoutParsingTrainer();
 
-var chat = api.Chat.CreateConversation();
-chat.AppendSystemMessage(training.SystemInstructions);
-foreach (var example in training.Training)
-{
-    chat.AppendUserInput(example.Input);
-    chat.AppendExampleChatbotOutput(example.ExpectedOutput);
-}
-chat.AppendUserInput(@"5 Rounds:
-- 10 Macebell 360's into Uppercut @30lb
-- 20 KB Swings @80lb
+var response = await container.Resolve<OpenAIAPI>()
+    .TrainAndAnswer(new WorkoutParsingTrainer(),
+    @"5 Rounds:
+    - 10 Macebell 360's into Uppercut @30lb
+    - 20 KB Swings @80lb
 
-Buyout: 
-- 100 burpees.
-");
+    Buyout: 
+    - 100 burpees.
+    ");
 
-string response = await chat.GetResponseFromChatbotAsync();
 Console.WriteLine(response); // "Yes"
-Console.ReadKey();
---
+++ OpenAIGettingStarted/TrainingClass.cs
@@ -0,0 +1,19 @@
+﻿using OpenAI_API;
+using OpenAIGettingStarted;
+
+public static class TrainingClass
+{
+    public static async Task<string> TrainAndAnswer(this OpenAIAPI api, PromptTrainer trainer, string question)
+    {
+        var chat = api.Chat.CreateConversation();
+        chat.AppendSystemMessage(trainer.SystemInstructions);
+        foreach (var example in trainer.Training)
+        {
+            chat.AppendUserInput(example.Input);
+            chat.AppendExampleChatbotOutput(example.ExpectedOutput);
+        }
+        chat.AppendUserInput(question);
+
+        return await chat.GetResponseFromChatbotAsync();
+    }
+}
